83931bb43347f71c27a815582467099c99169a5c
emzeee <mz@archbtw.use>
2024-12-12 21:43:57 +0100

    worst code ever

--- day2.py
+++ day2.py
@@ -6,17 +6,27 @@ results = {
 }
 
 def check_direction(report: list) -> str:
-    if report[0] > report[1]:
-        return "decreasing"
-    if report[0] < report[1]:
+    result = {
+        "increasing": 0,
+        "decreasing": 0
+    }
+
+    for i in range(len(report)-1):
+        if report[i+1] > report[i]:
+            result["increasing"] += 1
+        if report[i+1] < report[i]:
+            result["decreasing"] += 1
+    
+    if result["increasing"] > result["decreasing"]:
         return "increasing"
-    if report[0] == report[1]:
-        return "flat"
+    else:
+        return "decreasing"
+
+
 
 for report in data:
     report = [int(x) for x in report.split()]
     direction = check_direction(report)
-    print(direction)
     for idx, value in enumerate(report[:-1]):
         if direction == "decreasing":
             if report[idx] < report[idx+1]:
@@ -43,33 +53,148 @@ for report in data:
 print(results["safe"] - results["unsafe"])
 
 # PART 2
-results2 = {
-    "safe": 0,
-    "unsafe": 0
-}
+# def check_report(report: list) -> bool:
+#     for idx, level in enumerate(report["report"][:-1]):
+#         if report["direction"] == "decreasing":
+#             if level < report["report"][idx+1]:
+                
 
 
 reports = list()
 for idx, report in enumerate(data):
     report = [int(x) for x in report.split()]
     direction = check_direction(report)
-    reports.append({"report": report, "alternatives": [], "result": None, "direction": direction, "problem_dampener": 0})
+    reports.append({"report": report, "bad_levels": [], "result": None, "direction": direction, "problem_dampener": 0})
 
 
-for idy,report in enumerate(reports):
+for idy, report in enumerate(reports):
+    report_flag = True
     for idx, value in enumerate(report["report"][:-1]):
         if report["direction"] == "decreasing":
-            if report[idx] < report[idx+1]:
-                if report["problem_dampener"] == 0:
-                    reports[idy]["problem_dampener"] += 1
-                    report_alt_1 = report["report"].copy()
-                    report_alt_2 = report["report"].copy()
-                    reports[idy]["alternatives"].append(report_alt_1.pop(idx))
-                    reports[idy]["alternatives"].append(report_alt_1.pop(idx+1))
-                else: 
-                    reports[idy]["result"] = False
+            if report["report"][idx] < report["report"][idx+1]:
+                reports[idy]["result"] = False
+                reports[idy]["bad_levels"].append(idx)
+                reports[idy]["bad_levels"].append(idx+1)
+                report_flag = False         
+                break
+            if report["report"][idx] == report["report"][idx+1]:
+                reports[idy]["result"] = False
+                reports[idy]["bad_levels"].append(idx)
+                reports[idy]["bad_levels"].append(idx+1)
+                report_flag = False 
+                break
+        
+        if report["direction"] == "increasing":
+            if report["report"][idx] > report["report"][idx+1]:
+                reports[idy]["result"] = False
+                reports[idy]["bad_levels"].append(idx)
+                reports[idy]["bad_levels"].append(idx+1)
+                report_flag = False 
+                break
+            if report["report"][idx] == report["report"][idx+1]:
+                reports[idy]["result"] = False
+                reports[idy]["bad_levels"].append(idx)
+                reports[idy]["bad_levels"].append(idx+1)
+                report_flag = False 
+                break
+        
+        if report["direction"] == "flat":
+                reports[idy]["result"] = False
+                reports[idy]["bad_levels"].append(idx)
+                reports[idy]["bad_levels"].append(idx+1)
+                report_flag = False 
+                break
+        
+        if abs(report["report"][idx] - report["report"][idx+1]) > 3:
+                reports[idy]["result"] = False
+                reports[idy]["bad_levels"].append(idx)
+                reports[idy]["bad_levels"].append(idx+1)
+                report_flag = False 
+                break
+    if report_flag:
+        reports[idy]["result"] = True
+
+results = {
+    "all": 1000,
+    "s": 0,
+    "un": 0
+}
+
+results_set = set()
+
+for report in reports:
+    report_flag = True
+
+    if report["result"]:
+        results_set.add(str(report["report"]))
+    else:
+        report1 = report["report"].copy()
+        report2 = report["report"].copy()
+        report1.pop(report["bad_levels"][0])
+        report2.pop(report["bad_levels"][1])        
+
+        for idx, value in enumerate(report1[:-1]):
+            if report["direction"] == "decreasing":
+                if report1[idx] < report1[idx+1]:
+                    results["un"] +=1
+                    report_flag = False 
                     break
-                
+                if report1[idx] == report1[idx+1]:
+                    report_flag = False 
+                    break
+            if report["direction"] == "increasing":
+                if report1[idx] > report1[idx+1]:
+                    report_flag = False 
+                    break
+                if report1[idx] == report1[idx+1]:
+                    report_flag = False 
+                    break
+            if report["direction"] == "flat":
+                if report1[idx] < report1[idx+1]:
+                    report_flag = False 
+                    break
+            if abs(report1[idx] - report1[idx+1]) > 3:
+                report_flag = False 
+                break
+
+        if report_flag:
+            results_set.add(str(report["report"]))
+        
+        report_flag = True
+        
+        for idx, value in enumerate(report2[:-1]):
+            if report["direction"] == "decreasing":
+                if report2[idx] < report2[idx+1]:
+                    report_flag = False 
+                    break
+                if report2[idx] == report2[idx+1]:
+                    report_flag = False 
+                    break
+            if report["direction"] == "increasing":
+                if report2[idx] > report2[idx+1]:
+                    report_flag = False 
+                    break
+                if report2[idx] == report2[idx+1]:
+                    report_flag = False 
+                    break
+            if report["direction"] == "flat":
+                if report2[idx] > report2[idx+1]:
+                    report_flag = False 
+                    break
+            if abs(report2[idx] - report2[idx+1]) > 3:
+                report_flag = False 
+                break
+
+        if report_flag:
+            results_set.add(str(report["report"]))
+
+
+print(len(results_set))
+
+
+
+
+        
             
 
 
